208c30eba63a8087ec6d995d387962aaf589ee57
MrArivazhagan <arivazhagan.1011d@gmail.com>
2024-09-05 21:43:14 +0530

    Implement Add button and close option, day-17, 173

--- gui.py
+++ gui.py
@@ -2,11 +2,23 @@ import functions
 import FreeSimpleGUI as fsg
 
 label = fsg.Text("Type in a to-do")
-input_box = fsg.InputText(tooltip="Enter todo")
-add_button = fsg.Button("ADD")
+input_box = fsg.InputText(tooltip="Enter todo", key="todo")
+add_button = fsg.Button("Add")
 
-window = fsg.Window("To-do App", layout=[[label], [input_box, add_button]])
-window.read()
-# program stops in windows.read(), only after closing next lines will execute
-print("Window is closed")
+window = fsg.Window("To-do App",
+                    layout=[[label], [input_box, add_button]],
+                    font=('Helvetica', 20))
+
+while True:
+    event, values = window.read()
+    print(event, values, sep = "\n")
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            todos.append(values["todo"] + "\n")
+            functions.write_todos(todos)
+        # the close button click
+        case fsg.WIN_CLOSED:
+            break
+
 window.close()
