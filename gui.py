76e56514e5c0417e64f153ec63e0ac758c4ed992
MrArivazhagan <arivazhagan.1011d@gmail.com>
2024-09-05 23:18:49 +0530

    Implement Edit button, day-17, 174

--- gui.py
+++ gui.py
@@ -5,18 +5,35 @@ label = fsg.Text("Type in a to-do")
 input_box = fsg.InputText(tooltip="Enter todo", key="todo")
 add_button = fsg.Button("Add")
 
+todos_list = fsg.Listbox(values=functions.get_todos(), key="todos",
+                         size=(45, 10), enable_events=True)
+edit_button = fsg.Button("Edit")
+
+
 window = fsg.Window("To-do App",
-                    layout=[[label], [input_box, add_button]],
+                    layout=[[label], [input_box, add_button], [todos_list, edit_button]],
                     font=('Helvetica', 20))
 
 while True:
     event, values = window.read()
     print(event, values, sep = "\n")
+
     match event:
         case "Add":
             todos = functions.get_todos()
             todos.append(values["todo"] + "\n")
             functions.write_todos(todos)
+            window["todos"].update(values=functions.get_todos())
+        case "Edit":
+            todos = functions.get_todos()
+            edited_todo = values["todo"] + "\n"
+            index = todos.index(values["todos"][0])
+            todos[index] = edited_todo
+            functions.write_todos(todos)
+            # Accessing todos_list with it's key
+            window["todos"].update(values=functions.get_todos())
+        case "todos":
+            window["todo"].update(value=values["todos"][0].strip("\n"))
         # the close button click
         case fsg.WIN_CLOSED:
             break
