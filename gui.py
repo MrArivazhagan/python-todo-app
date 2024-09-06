c20da2c736abf298691a6b6d9e99ab769c596139
MrArivazhagan <arivazhagan.1011d@gmail.com>
2024-09-06 10:32:28 +0530

    implement complete and exit button [173, day-18]

--- gui.py
+++ gui.py
@@ -9,9 +9,14 @@ todos_list = fsg.Listbox(values=functions.get_todos(), key="todos",
                          size=(45, 10), enable_events=True)
 edit_button = fsg.Button("Edit")
 
+complete_button = fsg.Button("Complete")
+exit_button = fsg.Button("Exit")
 
 window = fsg.Window("To-do App",
-                    layout=[[label], [input_box, add_button], [todos_list, edit_button]],
+                    layout=[[label],
+                            [input_box, add_button],
+                            [todos_list, edit_button, complete_button],
+                            [exit_button]],
                     font=('Helvetica', 20))
 
 while True:
@@ -32,6 +37,16 @@ while True:
             functions.write_todos(todos)
             # Accessing todos_list with it's key
             window["todos"].update(values=functions.get_todos())
+        case "Complete":
+            todos = functions.get_todos()
+            index = todos.index(values["todos"][0])
+            todos.pop(index)
+            functions.write_todos(todos)
+            window["todos"].update(values=functions.get_todos())
+            # update in input box
+            window["todo"].update(value="")
+        case "Exit":
+            break
         case "todos":
             window["todo"].update(value=values["todos"][0].strip("\n"))
         # the close button click
