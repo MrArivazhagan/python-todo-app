a6fcd1d3d15146e1e28ff3a80202fd67ca5eee8a
MrArivazhagan <arivazhagan.1011d@gmail.com>
2024-09-06 10:06:04 +0530

    feet&inches to meters convertor with column alignment(FAQ 170), day-17

--- coding exercises/pro167/feet_inch_to_meter.py
+++ coding exercises/pro167/feet_inch_to_meter.py
@@ -12,9 +12,20 @@ inch_input_box = fsg.Input(key="inches")
 convert_button = fsg.Button("convert", key="convert")
 output_text = fsg.Text(key="result")
 
-window = fsg.Window(title="Convertor", layout=[[feet_label,feet_input_box],
-                                        [inch_label, inch_input_box],
-                                        [convert_button, output_text]])
+left_column_content = [[feet_label],
+                       [inch_label],
+                       [convert_button]]
+
+right_column_content = [[feet_input_box],
+                        [inch_input_box],
+                        [output_text]]
+
+left_column = fsg.Column(layout=left_column_content)
+right_column = fsg.Column(layout=right_column_content)
+
+layout = [[left_column, right_column]]
+
+window = fsg.Window(title="Convertor", layout=layout)
 
 while True:
     event, values = window.read()
